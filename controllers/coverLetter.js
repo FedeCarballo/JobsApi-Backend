c49ba549a9f571381df3bcb9c65b83c1cabe172b
FedeCarballo <carballo523@gmail.com>
2023-05-24 23:48:04 -0300

    Update coverLetter.js

---
+++ controllers/coverLetter.js
@@ -0,0 +1,25 @@
+const Letter = require('../models/CoverLetter')
+const {StatusCodes} = require('http-status-codes')
+const {BadRequestError, NotFoundError} = require('../errors')
+
+const CreateLetter = async (req,res) =>{
+
+}
+const GetLetter = async(req,res) => {
+
+}
+
+const UpdateLetter = async (req,res) => {
+
+}
+
+const DeleteLetter = async (req,res) =>{
+
+}
+
+module.exports = {
+    CreateLetter, 
+    GetLetter,
+    UpdateLetter,
+    DeleteLetter
+}
