ec5adbec2715a0aae8978c0680bcd6aa575cab85
FedeCarballo <carballo523@gmail.com>
2023-05-30 22:00:16 -0300

    logica recuperacion de contraseña

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const User = require('../models/User')
 const {StatusCodes} = require('http-status-codes')
+const nodemailer = require('nodemailer')
 const {BadRequestError, UnauthenticatedError} = require('../errors')
 const register = async (req,res) => {
     const user = await User.create({...req.body})
@@ -25,8 +26,38 @@ const login = async (req,res) => {
     const token = user.CreateJWT()
     res.status(StatusCodes.OK).json({user:{name: user.name}, token})
 }
-
+const ReqPassword = async (req,res) => {
+    const { email } = req.body;
+    const user = await User.findOne({ email })
+    if(!user){
+        throw new UnauthenticatedError('No se encontro un correo registrado')
+    }
+    const token = user.CreateJWT()
+    
+    const transporter = nodemailer.createTransport({
+        service:'Gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASS,
+        }
+    })
+    const mailOptions = {
+        from: 'tu_correo@gmail.com',
+        to: email,
+        subject: 'Restablecimiento de contraseña',
+        text: 'Haz clic en el siguiente enlace para restablecer tu contraseña: ' +
+          `http://localhost:3030/reset-password/${token}`,
+      };
+      transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+              console.log(error);
+              return res.status(500).json({ message: 'Ocurrió un error al enviar el correo electrónico.' });
+            }
+        });
+    res.status(StatusCodes.OK).json({ message: 'Se ha enviado un correo electrónico para restablecer la contraseña.' })
+}
 module.exports = {
     register,
-    login
+    login,
+    ReqPassword
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-const {login, register} = require('../controllers/auth')
+const {login, register, ReqPassword} = require('../controllers/auth')
 
 router.post('/register',register)
 router.post('/login',login)
-
+router.post('/forgot-password',ReqPassword)
 module.exports = router
