9e6c82a9d5dc12ba31337e9f75cf315f16701cbd
FedeCarballo <carballo523@gmail.com>
2023-06-04 14:20:11 -0300

    Back end terminado

--- controllers/coverLetter.js
+++ controllers/coverLetter.js
@@ -4,7 +4,6 @@ const {BadRequestError, NotFoundError} = require('../errors')
 
 const CreateLetter = async (req,res) =>{
     req.body.createdBy = req.user.userId
-    console.log(req.body.createdBy);
     const letter = await Letter.create(req.body)
     res.status(StatusCodes.CREATED).json({letter})
 }
@@ -15,17 +14,40 @@ const GetLetter = async(req,res) => {
         createdBy:userId 
     })
     if(!letter){
-        throw new NotFoundError(`Not job with ${JobId}`)
+        throw new NotFoundError(`No se encontro carta de presentacion con el id: ${LetterId}, por favor verifique los datos ingresados`)
     }
     res.status(StatusCodes.OK).json({letter})
 }
 
 const UpdateLetter = async (req,res) => {
-
+    const {
+        body: { description },
+        user:{ userId },
+        params:{ id:LetterId }} 
+        = req
+    if(description=== '' || !description){
+        throw new BadRequestError('Por favor rellene los datos solicitados')
+    }
+    const LetterUpdated = await Letter.findByIdAndUpdate({_id:LetterId, createdBy:userId}, req.body,{new:true, runValidators: true})
+    if(!LetterUpdated){
+        throw new NotFoundError(`No se encontro la carta de presentacion con el id: ${LetterId}`)
+    }
+    res.status(StatusCodes.OK).json({LetterUpdated})
 }
 
 const DeleteLetter = async (req,res) =>{
-
+    const {
+        user:{ userId },
+        params:{ id:LetterId }} 
+        = req
+    const job = await Letter.findByIdAndRemove({
+        _id: LetterId,
+        createdBy:userId,
+    })
+    if(!job){
+        throw new NotFoundError(`No se encontro carta de presentacion con el id: ${LetterId}`)
+    }
+    res.status(StatusCodes.OK).json({message: 'Carta de presentacion borrada exitosamente'})
 }
 
 module.exports = {
--- middleware/authentication.js
+++ middleware/authentication.js
@@ -4,22 +4,23 @@ const { UnauthenticatedError } = require('../errors')
 const auth = async (req,res, next) => {
     const authHeader = req.headers.authorization
     if(!authHeader || !authHeader.startsWith('Bearer ')){
-        throw new UnauthenticatedError('Authentication Invalid')
+        throw new UnauthenticatedError('Error en la autenticación')
     }
     //Hacemos el split y tomamos el segundo valor ya que es el que nos interesa
     const token = authHeader.split(' ')[1]
     const token2 = authHeader.split()[1]
     //Tenemos 2 formas de traer token por lo tanto verificamos ambas formas para poder validar el usuario
-    console.log("token line 11 authentication: ", token);
     if(token || token2){
         try {
             const payload = jwt.verify(token || token2, process.env.JWT_SECRET)
-    
             req.user = {userId:payload.userId, name:payload.name}
             next()
         } catch (error) {
-            throw new UnauthenticatedError('Authentication Invalid')
+            throw new UnauthenticatedError('Error en la autenticación')
         }
+    }
+    else{
+        throw new UnauthenticatedError('Error en la autenticación')
     }
 }
 
